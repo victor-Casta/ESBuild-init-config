5ed3308c438cdced808fa269f22097e57c0e9215
Victor Casta <victorarroyavecastaneda@gmail.com>
2023-08-15 11:03:23 -0500

    configure page an new files

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/app';
+
+ReactDOM.render(
+    <App message="Platzi ESBuid Example" />,
+    document.getElementById("app")
+    );
